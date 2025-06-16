df03df0cbfa782e4e04020216933f556c2e3fa4c
angelo <batiancelafrancis747@gmail.com>
2025-06-16 11:45:24 +0800

    added logic to get computer choice

--- js.js
+++ js.js
@@ -1,2 +1,29 @@
-console.log("Hello World!");
-alert("Hello World!");
+let computerChoice = "";
+let humanChoice = 0;
+
+
+function getRandom (max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getStance () {
+    let getNumber = getRandom(3);
+
+    if (getNumber === 0) {
+        computerChoice = "rock";
+    }
+    else if (getNumber === 1) {
+        computerChoice = "paper";
+    }
+    else if (getNumber === 2) {
+        computerChoice = "scissors";
+    }
+    else {
+        computerChoice = "invalid choice";
+    }
+
+    return computerChoice;
+}
+
+getStance();
+console.log(computerChoice);
