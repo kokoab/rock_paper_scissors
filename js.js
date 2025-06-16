21d7b27c9ddcb22a02dc7adca87600b80d82b729
angelo <batiancelafrancis747@gmail.com>
2025-06-16 12:07:11 +0800

    added computerChoice, humanChoice, playGame funct

--- js.js
+++ js.js
@@ -13,41 +13,7 @@ function playGame() {
         return prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
     }
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        if (humanChoice === computerChoice) {
-            console.log("It's a tie! Both chose " + humanChoice);
-            return;
-        }
-        if (
-            (humanChoice === "rock" && computerChoice === "scissors") ||
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper")
-        ) {
-            humanScore++;
-            console.log("You win! " + humanChoice + " beats " + computerChoice);
-        } else {
-            computerScore++;
-            console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    console.log("Game Over!");
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-    if (humanScore > computerScore) {
-        console.log("You won the game!");
-    } else if (computerScore > humanScore) {
-        console.log("Computer won the game!");
-    } else {
-        console.log("It's a tie game!");
-    }
+    
 }
 
 // Start the game
